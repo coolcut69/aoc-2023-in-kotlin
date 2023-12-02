ee384c2251456ba326616e7d2746659552baddcd
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-02 11:05:08 +0100

    Solution day 02 - part two

--- src/Day02.kt
+++ src/Day02.kt
@@ -5,15 +5,15 @@ private fun String.numberOfCubes(color: String): Int {
 
 fun main() {
     fun isGamePossible(sets: List<String>): Boolean {
+        //only 12 red cubes, 13 green cubes, and 14 blue cubes
         return !sets.any { it.numberOfCubes("blue") > 14 || it.numberOfCubes("red") > 12 || it.numberOfCubes("green") > 13 }
     }
 
     fun part1(input: List<String>): Int {
-        //only 12 red cubes, 13 green cubes, and 14 blue cubes
         var total: Int = 0
-        for (s in input) {
-            val gameNumber = s.split(':')[0].substringAfter("Game ").toInt()
-            val sets = s.split(':')[1].split(';')
+        for (games in input) {
+            val gameNumber = games.split(':')[0].substringAfter("Game ").toInt()
+            val sets = games.split(':')[1].split(';')
             if (isGamePossible(sets)) {
                 total += gameNumber
             }
@@ -22,15 +22,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var total = 0
+        for (games in input) {
+            val sets = games.split(':')[1].split(';')
+            val maxBlue = sets.stream().map { set -> set.numberOfCubes("blue") }.toList().max()
+            val maxGreen = sets.stream().map { set -> set.numberOfCubes("green") }.toList().max()
+            val maxRed = sets.stream().map { set -> set.numberOfCubes("red") }.toList().max()
+            total += maxBlue * maxGreen * maxRed
+        }
+        return total
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     check(part1(input) == 2169)
-    part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
