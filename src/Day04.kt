b9f59213b4940d67eb22d69b4352a4ae23783970
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-04 22:26:09 +0100

    Solution day 04 - part two - thanks to https://todd.ginsberg.com/post/advent-of-code/2023/day4/

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,5 +1,36 @@
 import kotlin.math.pow
 
+
+data class CardWithValue(val number: Int, val value: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CardWithValue) return false
+
+        if (number != other.number) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return number
+    }
+}
+
+private fun parseCard(input: String): Int {
+    val winningNumbers = input.substringAfter(":")
+        .substringBefore("|")
+        .split(" ")
+        .filter { it.isNotEmpty() }
+        .toSet()
+
+    val ourNumbers = input.substringAfter("|")
+        .split(" ")
+        .filter { it.isNotEmpty() }
+        .toSet()
+
+    return winningNumbers.intersect(ourNumbers).size
+}
+
 fun main() {
     fun getNumbers(s1: String): List<Int> {
         return s1.substring(1).trimEnd().chunked(3).map { s -> s.trim() }.map { s -> s.toInt() }
@@ -17,15 +48,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val cardMatches = input.map { parseCard(it) }
+
+        val cards = IntArray(cardMatches.size) { 1 }
+        cardMatches.forEachIndexed { index, score ->
+            repeat(score) {
+                cards[index+it+1] += cards[index]
+            }
+        }
+        return cards.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 13)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 30)
 
     val input = readInput("Day04")
     check(part1(input) == 23847)
-//    part2(input).println()
+    check(part2(input) == 8570000)
 }
