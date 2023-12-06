8dcdd9e9af1fea3123a59f1bd3d9f9b5e66b35d0
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-06 18:21:37 +0100

    Solution day 06 - part one

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,18 +1,28 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-       return input.size
+    fun part1(times: List<Int>, distance: List<Int>): Int {
+        val total: MutableList<Int> = ArrayList()
+        for ((index, time) in times.withIndex()) {
+            var numberOfPossible = 0
+            for (i in 0..time) {
+                val distanceBoat = i * (time - i)
+                if (distanceBoat > distance[index]) {
+                    numberOfPossible++
+                }
+            }
+            total.add(numberOfPossible)
+        }
+        return total.reduce { accumulator, element ->
+            accumulator * element
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(timeList: List<Int>, distance: List<Int>): Int {
+        return 1
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
-    check(part2(testInput) == 1)
+    check(part1(listOf(7, 15, 30), listOf(9, 40, 200)) == 288)
+//    check(part2(listOf(7, 15, 30), listOf(9, 40, 200)) == 71503)
 
-    val input = readInput("Day06")
-    part1(input).println()
-    part2(input).println()
+    part1(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)).println()
+//    part2(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)).println()
 }
