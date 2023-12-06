faa89fda03beb200e22e40730c8988eb666c0a77
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-06 18:39:10 +0100

    Solution day 06 - part two

--- src/Day06.kt
+++ src/Day06.kt
@@ -16,13 +16,24 @@ fun main() {
         }
     }
 
-    fun part2(timeList: List<Int>, distance: List<Int>): Int {
-        return 1
+    fun part2(time: Long, bestDistance: Long): Long {
+        var numberOfPossible = 0L
+        for (i in 0..time) {
+            val distanceBoat = i * (time - i)
+            if (distanceBoat > bestDistance) {
+                numberOfPossible++
+            }
+        }
+        return numberOfPossible
+
     }
 
-    check(part1(listOf(7, 15, 30), listOf(9, 40, 200)) == 288)
-//    check(part2(listOf(7, 15, 30), listOf(9, 40, 200)) == 71503)
 
-    part1(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)).println()
-//    part2(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)).println()
+    check(part1(listOf(7, 15, 30), listOf(9, 40, 200)) == 288)
+    check(part2(71530, 940200) == 71503L)
+
+    check(part1(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)) == 219849)
+    check(part2(44707080, 283113411341491) == 29432455L)
 }
+
+
