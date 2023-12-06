16006f28bfa39e8230c8f2803a69c56e68e77915
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-06 18:43:34 +0100

    Solution day 06 - refactor

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,15 +1,19 @@
 fun main() {
-    fun part1(times: List<Int>, distance: List<Int>): Int {
-        val total: MutableList<Int> = ArrayList()
-        for ((index, time) in times.withIndex()) {
-            var numberOfPossible = 0
-            for (i in 0..time) {
-                val distanceBoat = i * (time - i)
-                if (distanceBoat > distance[index]) {
-                    numberOfPossible++
-                }
+    fun i(time: Long, distance: Long): Long {
+        var numberOfPossible = 0L
+        for (i in 0..time) {
+            val distanceBoat = i * (time - i)
+            if (distanceBoat > distance) {
+                numberOfPossible++
             }
-            total.add(numberOfPossible)
+        }
+        return numberOfPossible
+    }
+
+    fun part1(times: List<Long>, distance: List<Long>): Long {
+        val total: MutableList<Long> = ArrayList()
+        for ((index, time) in times.withIndex()) {
+            total.add(i(time, distance[index]))
         }
         return total.reduce { accumulator, element ->
             accumulator * element
@@ -17,23 +21,14 @@ fun main() {
     }
 
     fun part2(time: Long, bestDistance: Long): Long {
-        var numberOfPossible = 0L
-        for (i in 0..time) {
-            val distanceBoat = i * (time - i)
-            if (distanceBoat > bestDistance) {
-                numberOfPossible++
-            }
-        }
-        return numberOfPossible
-
+        return i(time, bestDistance)
     }
 
 
-    check(part1(listOf(7, 15, 30), listOf(9, 40, 200)) == 288)
+    check(part1(listOf(7, 15, 30), listOf(9, 40, 200)) == 288L)
     check(part2(71530, 940200) == 71503L)
 
-    check(part1(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)) == 219849)
+    check(part1(listOf(44, 70, 70, 80), listOf(283, 1134, 1134, 1491)) == 219849L)
     check(part2(44707080, 283113411341491) == 29432455L)
 }
-
 
