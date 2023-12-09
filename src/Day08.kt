b40a54d5f85284ee4cbb06ce76a95671235ad268
cborghmans <chris.borghmans@dpgmedia.be>
2023-12-09 17:13:31 +0100

    Solution day 08 - part two, tnx to xiaowuc1

--- src/Day08.kt
+++ src/Day08.kt
@@ -5,10 +5,16 @@ private fun parseRouteMap(input: List<String>): Map<String, Pair<String, String>
             it.substring(0..2) to (it.substring(7..9) to it.substring(12..14))
         }
 
-fun main() {
+fun gcd(a: Long, b: Long): Long {
+    if(b == 0L) {
+        return a
+    }
+    return gcd(b, a%b)
+}
 
+fun main() {
     fun part1(input: List<String>): Int {
-        val instructions = input.get(0)
+        val instructions = input[0]
         val map = parseRouteMap(input)
 
         var steps: Int = 0
@@ -23,20 +29,45 @@ fun main() {
                 }
                 steps++
             }
-        } while (place != "ZZZ")
+        } while (!place.endsWith( "Z"))
         return steps
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val instructions = input[0]
+        val edges = HashMap<String, List<String>>()
+        for(i in 2 until input.size) {
+            val line = input[i].split(" ")
+            val node = line[0]
+            var lhs = line[2]
+            lhs = lhs.substring(1, lhs.length - 1)
+            var rhs = line[3]
+            rhs = rhs.substring(0, rhs.length-1)
+            edges[node] = listOf(lhs, rhs)
+        }
+        var ret: Long = 1
+        for(cand in edges.keys) {
+            if(cand[cand.length-1] != 'A') continue
+            var len: Long = 0
+            var now = cand
+            while(now[now.length-1] != 'Z') {
+                val go = instructions[(len % instructions.length).toInt()]
+                val idx = if(go == 'L') 0 else 1
+                now = edges[now]!![idx]
+                len += 1
+            }
+            ret = ret / gcd(ret, len) * len
+        }
+        return ret
     }
+
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 6)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 6L)
 
     val input = readInput("Day08")
     check(part1(input) == 17141)
-//    part2(input).println()
+    check(part2(input) == 10818234074807)
 }
